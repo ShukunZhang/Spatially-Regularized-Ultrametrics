3afaba26f66a325247fbd6f9fed7fdfc7c7f9180
Shukun_Zhang <34422187+ShukunZhang@users.noreply.github.com>
2020-10-02 04:46:18 +0800

    Update ThreeCubes_All.m

--- SRUSC/RunAll/ThreeCubes_All.m
+++ SRUSC/RunAll/ThreeCubes_All.m
@@ -1,69 +1,72 @@
 close all;
 clear all;
-X=zeros(1000,3);
+num=20;
+X=zeros(num^3,3);
 
-for i=1:10
-    for j=1:10
-        for k=1:10
-            X((i-1)*100+(j-1)*10+k,1) = i/10;
-            X((i-1)*100+(j-1)*10+k,2) = j/10;
-            X((i-1)*100+(j-1)*10+k,3) = k/10;
+for i=1:num
+    for j=1:num
+        for k=1:num
+            X((i-1)*num^2+(j-1)*num+k,1) = i/num;
+            X((i-1)*num^2+(j-1)*num+k,2) = j/num;
+            X((i-1)*num^2+(j-1)*num+k,3) = k/num;
         end
     end
 end
 X=vertcat(X,X,X);
+scatter3(X(:,1),X(:,2),X(:,3),[],'filled');
 
-X([1001:2000],3) = X([1001:2000],3)+1.2;
-X([2001:3000],3) = X([2001:3000],3)+2.4;
+X([num^3+1:2*(num^3)],3) = X([num^3+1:2*(num^3)],3)+1.5;
+X([2*num^3+1:3* num^3],3) = X([2*num^3+1:3* num^3],3)+3;
 
+scatter3(X(:,1),X(:,2),X(:,3),[],'filled');
+X=horzcat(X,zeros(3*num^3,196));
  
- X=horzcat(X,zeros(3000,196));
- 
-
- [Q,R] = qr(randn(199));
- X=X*Q;
- X=horzcat(X,ones(3000,1));
- for i=1:3
-     for j=1:1000
-         X((i-1)*1000 + j,200) = i/10; 
+[Q,R] = qr(randn(199));
+X=X*Q;
+X=horzcat(X,ones(3*num^3,1));
+for i=1:3
+     for j=1:num^3
+         X((i-1)*num^3 + j,200) = i/10; 
      end
  end
 
-%  
-C_1=X(randi([500 600],30,1),:);
-C_2=X(randi([2500 2600],30,1),:);
- 
-X(randi([500 600],30,1),:)=C_2;
-X(randi([2500 2600],30,1),:) = C_1;
 
+C_1=randi([num^3/2 num^3/2+100],10,1);
+C_2=randi([2*num^3+num^3/2 2*num^3+num^3/2+100],10,1);
+  
+temp =  X(C_1,:);
+X(C_1,:) = X(C_2,:);
+X(C_2,:) = temp;
 
 addpath(genpath('../../SRUSC'));
-load('ThreeCubes_gt.mat');
-LabelsGT=TCGT;
+load('TC_GT_100x240.mat');
+
 SetDefaultParameters 
 DenoisingOpts.Method='None';
  
-%using spatial 
-SRUSCopts.KNN = 1400; 
+ %using spatial 
+SRUSCopts.KNN = 8100; 
 SpectralOpts.SigmaScaling = 'Manual';
 SpectralOpts.NumEig = 10;
-SpectralOpts.SigmaValues = linspace(0.01, 10, 10);
+SpectralOpts.SigmaValues = linspace(1,10,5);
+
 
 
 SpatialReg.UseReg = 1;
-SpatialReg.Width = 50;
-SpatialReg.Height = 60;
+SpatialReg.Width = 240;
+SpatialReg.Height = 100;
 
-SpatialReg.r=30;
+SpatialReg.r=50;
 MajorV.Use = 0;
 
  
-ComparisonOpts.RunEucSC = 1;
+ComparisonOpts.RunEucSC = 0;
 ComparisonOpts.Kmeans = 0;
 ComparisonOpts.EucSCSigmaScaling = 'Automatic'; 
-
-GeneralScript_SRUSC
-
+% 
+% 
+% 
+  GeneralScript_SRUSC
 
 %% Kmeans
 tic;
@@ -76,9 +79,9 @@ Time_Kmeans=toc;
 tic;
 V=GetPC(X);
 Labels_PCA=kmeans(X*V(:,1:2),K,'Replicates', SpectralOpts.NumReplicates)';
-Labels_PCA=reshape(Labels_PCA,50*60,1);
+Labels_PCA=reshape(Labels_PCA,24000,1);
 Time_PCA=toc;
-[OA_PCA,AA_PCA,Kappa_PCA]=GetAccuracies(Labels_PCA(LabelsGT>0),reshape(LabelsGT,50*60,1),K);
+[OA_PCA,AA_PCA,Kappa_PCA]=GetAccuracies(Labels_PCA(LabelsGT>0),reshape(LabelsGT,24000,1),K);
 
 
 %% Compute Gaussian Mixture Model
@@ -86,14 +89,14 @@ tic;
 gmfit=fitgmdist(X,K, 'CovarianceType', 'full',  'RegularizationValue', 1e-10);
 Labels_GMM= cluster(gmfit,X);
 Time_GMM=toc;
-[OA_GMM,AA_GMM,Kappa_GMM]=GetAccuracies(Labels_GMM(LabelsGT>0),reshape(LabelsGT,50*60,1),K);
+[OA_GMM,AA_GMM,Kappa_GMM]=GetAccuracies(Labels_GMM(LabelsGT>0),reshape(LabelsGT,24000,1),K);
 
 
 %% Compute Hierarchical NMF
 tic;
 Labels_NMF=hierclust2nmf((X-min(min(X)))',K);
 Time_NMF=toc;
-[OA_NMF,AA_NMF,KappaNMF]=GetAccuracies(Labels_NMF(LabelsGT>0),reshape(LabelsGT,50*60,1),K);
+[OA_NMF,AA_NMF,KappaNMF]=GetAccuracies(Labels_NMF(LabelsGT>0),reshape(LabelsGT,24000,1),K);
 
 
 
@@ -111,8 +114,8 @@ K_GT=max(Y);
     
 Y=LabelsGT';  
 %X=permute(X,[2,1,3]); 
-M=50;
-N=60;
+M=100;
+N=240;
 PeakOpts.Window_Diffusion = 20;
 
 
@@ -164,7 +167,7 @@ tic;
 PeakOpts.ModeDetection='Diffusion';
 [CentersDiffusion, G_dif, DistStructDiffusion] = DensityPeaksEstimation(X, K_Learned, DensityNN, PeakOpts, M, N);
 Labels_DL=FSFclustering(X,K,DistStructDiffusion,CentersDiffusion);
-Labels_DL=reshape(Labels_DL,50*60,1);
+Labels_DL=reshape(Labels_DL,24000,1);
 Time_DL=Time_temp+toc;
 % 
 
@@ -174,7 +177,7 @@ tic;
 PeakOpts.ModeDetection='Euclidean';
 [CentersEuclidean, G_euc, DistStructEuclidean] = DensityPeaksEstimation(X, K_Learned, DensityNN, PeakOpts, M, N);
 Labels_FSFDPC=FSFclustering(X,K_GT,DistStructEuclidean,CentersEuclidean);
-Labels_FSFDPC=reshape(Labels_FSFDPC,50*60,1);
+Labels_FSFDPC=reshape(Labels_FSFDPC,24000,1);
 Time_FSFDPC=Time_temp+toc;
 % 
 % % Diffusion distances with spatial regularization
@@ -188,7 +191,7 @@ Time_FSFDPC=Time_temp+toc;
 
 %% Compute LCMR
 
-[RD_hsi]= reshape(X,50,60,200);
+[RD_hsi]= reshape(X,240,100,200);
 
 load('ThreeCubes_gt.mat');
 labels=TCGT;
